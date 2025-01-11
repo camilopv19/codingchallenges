256302b6f08429ca719f41fd1d69e4aa0b914c31
Camilo P. <kmilopv@gmail.com>
2025-01-11 16:25:15 -0500

    Update Binary searches.cs

--- Binary searches.cs
+++ Binary searches.cs
@@ -1,5 +1,7 @@
-// Online C# Editor for free
-// Write, Edit and Run your C# code using C# Online Compiler
+// WCS: Worst Case Scenario
+// BCS: Best Case Scenario
+// NF: Best Case Scenario
+
 using System.Diagnostics;
 using System;
 using System.Linq;
@@ -9,57 +11,114 @@ public class BinarySearch
     public static void Main(string[] args)
     {
         var stopwatch = Stopwatch.StartNew();
-
-        int[] nums = new int[100]{ 432, 186, 488, 4, 216, 31, 416, 185, 347, 495, 496, 329, 99, 480, 146, 390, 160, 343, 318, 288, 6, 167, 483, 294, 48, 228, 274, 343, 434, 426, 89, 344, 337, 250, 348, 310, 425, 209, 188, 419, 135, 43, 371, 45, 215, 310, 443, 271, 5, 194, 270, 402, 114, 45, 31, 217, 290, 2, 74, 193, 55, 254, 246, 267, 28, 464, 375, 108, 97, 350, 327, 184, 10, 26, 489, 170, 53, 483, 118, 90, 5, 369, 115, 33, 350, 184, 403, 183, 339, 355, 417, 105, 323, 58, 185, 44, 402, 142, 457, 233 }; // O(1)
-
-      // For BS to work proper and correctly, array MUST be sorted.
-        Array.Sort(nums);   // O(n) time
-        printArr(nums);     // O(n) time
-        binarySearch(nums, 2);
+        int MAX_VALUE = 99_999_999;
+        int WCS_VALUE = 100_000_000;
+        int BCS_VALUE = 1;
+        int[] nums = Enumerable.Range(1, MAX_VALUE).ToArray(); // O(1)
+        
+        // For BS to work, array must be sorted
+        // Array.Sort(nums);   // O(n) time
+        // printArr(nums);     // O(n) time
+       
+        /*                                 
+        binarySearch(nums, MAX_VALUE);      // 00:00:00.5987489
+        binarySearch(nums, WCS_VALUE);      
+        binarySearch(nums, BCS_VALUE);
+        */
+        /*
+        linearSearch(nums, MAX_VALUE);      // 00:00:01.3701052
+        linearSearch(nums, WCS_VALUE);      
+        linearSearch(nums, BCS_VALUE);
+        */
+       
+        print(binarySearchRecursive(nums, MAX_VALUE));
+        print(binarySearchRecursive(nums, WCS_VALUE));
+        print(binarySearchRecursive(nums, BCS_VALUE));
+        
         stopwatch.Stop();
         
         var elapsed = stopwatch.Elapsed;
-        print($"{elapsed.ToString()} ms elapsed");
+        print($"{elapsed.ToString()} time elapsed");
     }
-
-        private static bool binarySearch(int[] arr, int target){
-            int size = arr.Length;
-            int start = 0;
-            int end = size - 1;
-            int idx = 0, mid = 0;
-            
-            while(start <= end){
-                mid = (int)(Math.Floor((double)((start + end) /2)));
-                
-                // First happy path: Target at middle position
-                if(arr[mid] == target) {
-                    print($"Value found at position {mid}");
-                    return true;
-                }
-                else if(target > arr[mid]){
-                    start = mid + 1;
-                }
-                else{
-                    end = mid - 1;
-                }
+    
+    private static void linearSearch(int[] arr, int target){
+        bool found = false;
+        int idx = 0;
+        
+        for(int i=0 ; i<arr.Length; i++){
+            if(arr[i]==target){
+                idx = i;
+                found = true;
+                break;
             }
-            print("Value not found");
+        }
+        print(found, idx);
+    }
+    
+    private static bool binarySearchRecursive(int[] arr, int target){
+        //printArr(arr);
+        int last = arr.Length - 1;
+        
+        // Break condition
+        if((arr.Length == 1 && arr[0] != target) || target > arr[last]) {
             return false;
+            
+        } 
+        else{
+            int mid = (int)(Math.Floor((double)(arr.Length/2)));
+            if(arr[mid] == target) {
+                return true;
+            }
+            else if(target > arr[mid]){
+                return binarySearchRecursive(arr[mid..], target);
+            }
+            else{
+                return binarySearchRecursive(arr[..mid], target);
+            }
         }
+    }
+    
+    private static void binarySearch(int[] arr, int target){
+        int size = arr.Length;
+        int start = 0;
+        int end = size - 1;
+        int idx = 0, mid = 0;
+        bool found = false;
         
-        private static void printArr(int[] arrInput){
-            //Array.Reverse(arrInput);
-            var num = string.Join(",", arrInput.Select(x => x.ToString()).ToArray());
-            // foreach(int num in arrInput){
-                Console.WriteLine($"{num}");
-            //}
+        while(start <= end){
+            mid = (int)(Math.Floor((double)((start + end) /2)));
+            
+            // First happy path: Target at middle position
+            if(arr[mid] == target) {
+                idx = mid;
+                found = true;
+                break;
+            }
+            else if(target > arr[mid]){
+                start = mid + 1;
+            }
+            else{
+                end = mid - 1;
+            }
         }
-        
-        private static void print(double input, string name){
-                Console.WriteLine($"{name}: {input}");
-        }
-        
-        private static void print(string msg){
-                Console.WriteLine($"{msg}");
-        }
+        print(found, idx);
+    }
+    
+    private static void printArr(int[] arrInput){
+        //Array.Reverse(arrInput);
+        var num = string.Join(",", arrInput.Select(x => x.ToString()).ToArray());
+        Console.WriteLine($"{num}");
+    }
+    
+    private static void print(bool found){
+        Console.WriteLine(found ? "Value found!" : "Value not found");
+    }
+    
+    private static void print(bool found, int idx){
+        Console.WriteLine(found ? "Value found at " + idx : "Value not found");
+    }
+    
+    private static void print(string msg){
+        Console.WriteLine($"{msg}");
+    }
 }
